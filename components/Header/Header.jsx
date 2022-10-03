217771180b28a024bfd7b67232a298530d8e2fd8
kawai011231 <yousuke011231@gmail.com>
2022-10-03 18:30:46 +0900

    ヘッダーの作成

---
+++ components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import classes from "./Header.module.css";
+
+export const Header = () => {
+  return (
+    <header className={classes.header}>
+      <h1 className={classes.h1}>掲示板作ってみました</h1>
+    </header>
+  );
+};
