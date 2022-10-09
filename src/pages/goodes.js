d3f61e658149818f00e58c063569ad498c439c26
kawai011231 <yousuke011231@gmail.com>
2022-10-09 13:07:05 +0900

    ぷろぱてぃーを追加

--- src/pages/goodes.js
+++ src/pages/goodes.js
@@ -12,7 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <Main title="うちらチョーかわいい" />
+      <Main title="うちらチョーかわいい" thread="goodes" />
     </div>
   );
 }
